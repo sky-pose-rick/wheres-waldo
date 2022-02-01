26e27d5b43713c98f59d1ad1ab93b943c9d3e198
Richard Cranwell <rickycranwell@gmail.com>
2022-02-01 16:41:25 -0600

    remove game mocking from app test

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 // mock components to prevent async calls/updates
-import Game from './Game';
 import Home from './Home';
 
-jest.mock('./Game');
 jest.mock('./Home');
 
 it('renders without crash', () => {
-  Game.mockImplementation(() => (<div>Game</div>));
   Home.mockImplementation(() => (<div>Home</div>));
   render(<App />);
 });
